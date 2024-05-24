cbaba06a96c602e31be4000707fa390009ed96cc
Rafael Silverio <rafael.silverio@anota.ai>
2024-05-24 14:33:32 -0300

    set config in main file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(proccess.env.PORT || 3000);
+  const configServices = app.get<ConfigService>(ConfigService);
+  await app.listen(configServices.get('PORT') || 3000);
 }
 bootstrap();
